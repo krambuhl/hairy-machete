b280d8ff82a17cfbb9d288c00922add41bdcc098
Evan Krambuhl <ekrambuhl@connectivedx.com>
2016-10-23 01:44:38 -0700

    basic page ?

--- source/pages/styleguide/index.jsx
+++ source/pages/styleguide/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
-import StyleguidePage from '../../styleguide/page.jsx';
+import PageRoot from '../../tags/page-root/page-root.jsx';
+import Rhythm from '../../tags/rhythm/rhythm.jsx';
 import Heading from '../../tags/heading/heading.jsx';
+import Wrapper from '../../tags/wrapper/wrapper.jsx';
+import ComponentList from '../../styleguide/component-list.jsx';
 
 export default ({ locals }) => (
-	<StyleguidePage title="Styleguide" locals={locals}>
-		<Heading>Why, Hello!</Heading>
-	</StyleguidePage>
+	<PageRoot title="Styleguide">
+		<Wrapper>
+			<Rhythm size="small" deep="true">
+				<Heading>Why, Hello!</Heading>
+				<Heading tagName="h2">Styleguide</Heading>
+
+				<Heading tagName="h3">Components</Heading>
+				<ComponentList baseUrl="/styleguide/components" components={locals.components} />
+				
+				<Heading tagName="h3">Tags</Heading>
+				<ComponentList baseUrl="/styleguide/tags" components={locals.tags} />
+			</Rhythm>
+	</Wrapper>
+	</PageRoot>
 );
